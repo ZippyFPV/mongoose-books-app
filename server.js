63742b5d6fc2c844bdcf997bd6480fd62446fddd
Benjamin Tovar <benjamintovar@benjamins-mbp.attlocal.net>
2017-06-04 18:58:19 -0500

    updated routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 /////////////////////////////
 //  SETUP and CONFIGURATION
 /////////////////////////////
-
+var db = require('./models')
 //require express in our app
 var express = require('express'),
   bodyParser = require('body-parser');
@@ -70,66 +70,69 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
-// get all books
+// server.js
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books){
+    if (err) {
+      console.log("index error: " + err);
+      res.sendStatus(500);
+    }
+    res.json(books);
+  });
 });
+
 
 // get one book
-app.get('/api/books/:id', function (req, res) {
-  // find one book by its id
-  console.log('books show', req.params);
-  for(var i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
+app.get('/api/books/:id', function(req, res) {
+    // get book id from url params (`req.params`)
+    var bookId = req.params.id;
+
+    // find todo in db by id
+    db.Book.findOne({ _id: bookId }, function(err, foundBook) {
+       	if(err){return console.log(err)}
+        res.json(foundBook);
+    });
 });
+
 
 // create new book
-app.post('/api/books', function (req, res) {
-  // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  newBook._id = newBookUUID++;
-  books.push(newBook);
-  res.json(newBook);
+app.post('/api/books', function(req, res) {
+    // create new todo with form data (`req.body`)
+    var newBook = req.body;
+    db.Book.create(newBook, function(err, createdBook) {
+    	if(err){return console.log(err)}
+    	res.json(createdBook);
+    });
 });
 
-// update book
-app.put('/api/books/:id', function(req,res){
-// get book id from url params (`req.params`)
-  console.log('books update', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var updateBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('updating book with index', deleteBookIndex);
-  var bookToUpdate = books[deleteBookIndex];
-  books.splice(updateBookIndex, 1, req.params);
-  res.json(req.params);
+// update todo
+app.put('/api/books/:id', function(req, res) {
+    // get book id from url params (`req.params`)
+    var bookId = req.params.id;
+
+    // create an updateBook object from req.body
+	var updateBook = {
+	  task: req.body.task,
+      description: req.body.description
+    }
+
+    db.Book.findOneAndUpdate({ _id: bookId }, updateBook, { new: true}, function(err, updatedBook){
+      if(err){return console.log(err)}
+	  res.json(updatedBook);
+    });
 });
 
-// delete book
-app.delete('/api/books/:id', function (req, res) {
-  // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var deleteBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('deleting book with index', deleteBookIndex);
-  var bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
+// delete todo
+app.delete('/api/books/:id', function(req, res) {
+    // get book id from url params (`req.params`)
+    var bookId = req.params.id;
+
+    // find book in db by id and remove
+    db.Book.findOneAndRemove({ _id: bookId }, function(err, deletedBook) {
+        res.json(deletedBook);
+    });
 });
-
-
 
 
 
