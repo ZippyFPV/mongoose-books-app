126940f7ab8b80974aab03e9f0d18cf6bb5e9ab2
Benjamin Tovar <benjamintovar@benjamins-mbp.attlocal.net>
2017-06-04 19:42:30 -0500

    Added author routes

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-// server.js
 // SERVER-SIDE JAVASCRIPT
 
-
-/////////////////////////////
-//  SETUP and CONFIGURATION
-/////////////////////////////
-var db = require('./models')
 //require express in our app
 var express = require('express'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  db = require('./models');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -19,123 +14,84 @@ app.use(express.static('public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-////////////////////
-//  DATA
-///////////////////
-
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
-var newBookUUID = 18;
-
-
-
-
-
-
-
-////////////////////
-//  ROUTES
-///////////////////
-
-
-
-
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
-// server.js
+// get all books
 app.get('/api/books', function (req, res) {
-  // send all books as JSON response
-  db.Book.find(function(err, books){
+  // find one book by its id
+  db.Book.find({})
+    .populate('author')
+    .exec(function(err, books){
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+      res.json(books);
+    });
+
+});
+
+
+
+app.get('/api/books/:id', function (req, res) {
+  // find one book by its id
+  db.Book.findById(req.params.id)
+    // populate the author
+    .populate('author')
+    .exec(function(err, book){
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+      res.json(book);
+    });
+
+});
+
+
+
+app.post('/api/books', function (req, res) {
+  // create new book with form data (`req.body`)
+  var newBook = new db.Book({
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
+  });
+  // find the author from req.body
+  db.Author.findOne({name: req.body.author}, function(err, author){
     if (err) {
-      console.log("index error: " + err);
-      res.sendStatus(500);
+      return console.log(err);
     }
-    res.json(books);
+    // add this author to the book
+    newBook.author = author;
+    // save newBook to database
+    newBook.save(function(err, book){
+      if (err) {
+        return console.log("save error: " + err);
+      }
+      console.log("saved ", book.title);
+      // send back the book!
+      res.json(book);
+    });
+  });
+
+});
+
+
+// delete book
+app.delete('/api/books/:id', function (req, res) {
+  // get book id from url params (`req.params`)
+  console.log(req.params)
+  var bookId = req.params.id;
+
+  db.Book.findOneAndRemove({ _id: bookId }, function (err, deletedBook) {
+    res.json(deletedBook);
   });
 });
 
-
-// get one book
-app.get('/api/books/:id', function(req, res) {
-    // get book id from url params (`req.params`)
-    var bookId = req.params.id;
-
-    // find todo in db by id
-    db.Book.findOne({ _id: bookId }, function(err, foundBook) {
-       	if(err){return console.log(err)}
-        res.json(foundBook);
-    });
-});
-
-
-// create new book
-app.post('/api/books', function(req, res) {
-    // create new todo with form data (`req.body`)
-    var newBook = req.body;
-    db.Book.create(newBook, function(err, createdBook) {
-    	if(err){return console.log(err)}
-    	res.json(createdBook);
-    });
-});
-
-// update todo
-app.put('/api/books/:id', function(req, res) {
-    // get book id from url params (`req.params`)
-    var bookId = req.params.id;
-
-    // create an updateBook object from req.body
-	var updateBook = {
-	  task: req.body.task,
-      description: req.body.description
-    }
-
-    db.Book.findOneAndUpdate({ _id: bookId }, updateBook, { new: true}, function(err, updatedBook){
-      if(err){return console.log(err)}
-	  res.json(updatedBook);
-    });
-});
-
-// delete todo
-app.delete('/api/books/:id', function(req, res) {
-    // get book id from url params (`req.params`)
-    var bookId = req.params.id;
-
-    // find book in db by id and remove
-    db.Book.findOneAndRemove({ _id: bookId }, function(err, deletedBook) {
-        res.json(deletedBook);
-    });
-});
-
-
-
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Book app listening at http://localhost:3000/');
+  console.log('Example app listening at http://localhost:3000/');
 });
